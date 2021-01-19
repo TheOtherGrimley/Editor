42064e77c95b7644e4ad1bde2dbf7b2e0fd48255
TheOtherGrimley <adamgrimley@outlook.com>
2021-01-19 17:03:13 +0000

    Changed lock keybind to ctrl+r, added clear console command

--- CustomShortcuts.cs
+++ CustomShortcuts.cs
@@ -1,12 +1,23 @@
 using UnityEditor;
- 
+using System;
+using System.Reflection;
+
 static class CustomShortcuts
 {
     // taken from: http://answers.unity3d.com/questions/282959/set-inspector-lock-by-code.html
-    [MenuItem("Tools/Toggle Inspector Lock %l")] // Ctrl + L
+    [MenuItem("Tools/Toggle Inspector Lock %r")] // Ctrl + L
     static void ToggleInspectorLock()
     {
         ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
         ActiveEditorTracker.sharedTracker.ForceRebuild();
+    }
+
+    [MenuItem("Tools/Clear Console &c")]
+    static void ClearConsole()
+    {
+        // This simply does "LogEntries.Clear()" the long way:
+        var logEntries = Type.GetType("UnityEditor.LogEntries,UnityEditor.dll");
+        var clearMethod = logEntries.GetMethod("Clear", BindingFlags.Static | BindingFlags.Public);
+        clearMethod.Invoke(null, null);
     }
 }
